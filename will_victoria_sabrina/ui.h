46f8b444accc9c870ea160480c322b82dc668a83
WFT <will.a.ft@gmail.com>
2014-01-13 20:43:17 -0500

    SDL window + makefile

---
+++ will_victoria_sabrina/ui.h
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+// logs any SDL error, taking op as the operation that failed
+void log_SDL_error(const char *op);
+
+// initialize SDL
+int init_SDL();
+
+// cleanup SDL
+void cleanup_SDL();
+
+// load texture
+SDL_Texture *load_texture(const char*);
