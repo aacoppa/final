35107faa5bad7bf1d9639e1a747b74cf06381b84
Aaron Coppa <aacoppa@gmail.com>
2014-01-15 21:55:30 -0500

    Added more skeleton for client side
    Game can be written in game.c

---
+++ Aaron_Jack_Sweyn/client/game.h
@@ -0,0 +1,16 @@
+/* Game: 
+ * -runs independently
+ * -doesn't worry about client/server
+ * -is called by client
+ */
+#ifndef __GAME_AJS
+#define __GAME_AJS 1
+
+typedef struct map {
+    /* Map 
+     *
+     */
+} map;
+void startGame();
+void run();
+#endif
---
+++ Aaron_Jack_Sweyn/client/generate_map.h
@@ -0,0 +1,5 @@
+#ifndef __GENERATE_MAP_AJS
+#define __GENERATE_MAP_AJS 1
+
+
+#endif
