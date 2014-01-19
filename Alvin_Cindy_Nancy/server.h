baf6e4b3d586e21c983b22bedba4e584797ff972
aleung013 <aleung013@gmail.com>
2014-01-19 15:19:57 -0500

    Add server files

---
+++ Alvin_Cindy_Nancy/server.h
@@ -0,0 +1,13 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#endif
