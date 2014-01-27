22ff5122f35d590308c4e17a3011ecd4556b54da
arfan308 <areha308@gmail.com>
2014-01-27 00:04:56 -0500

    swap method

---
+++ Arfan_Timothy/src/poker.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "poker.h"
+
+/* this method is where the core game logic exists */
+int start_game() {
+    
+    if (RUN_TESTS) return test_queue();  /* enable and disables tests */
+    Game * game = malloc(sizeof(Game));
+    init_game(game);
+    
+    while (!game_winner(game, game->player_count)) {
+        
+        /* deal players */
+        deal_players(game->players, game->deck);
+        
+        /* ante up */
+        ante_up(game);
+        
+        /* show hand to human player */
+        show_hands(game->players, game->player_count, TRUE);
+        
+        /* first round of betting */
+        betting_round(game, game->players, game->player_count);
+        
+        /* players exchange cards if any */
+        exchange_turn(game->players, game->player_count, game->deck);
+        
+        /* second round of betting */
+        betting_round(game, game->players, game->player_count);
+        
+        /* print winning hand and player */
+        determine_winner(game, game->players, game->player_count);
+    }
+    
+    return 0;
+    
+}
