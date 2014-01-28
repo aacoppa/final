f48279589d73b873e165550875f7d50aec9b05c6
arfan308 <areha308@gmail.com>
2014-01-27 21:49:45 -0500

    betting

--- Arfan_Timothy/src/game.c
+++ Arfan_Timothy/src/game.c
@@ -47,3 +47,94 @@ void ante_up(Game * game){
     printf("Pot at: %d\n", game->pot);
 }
 
+/* called twice, once before exchange, once after */
+void betting_round(Game * game, Player * players, int player_size){
+    int i;
+    i = 0;
+    char * result = malloc(sizeof(char) * 10);
+    int bet;
+    int count;
+    
+    /* check to make sure everyone didn't fold */
+    /* if everyone has folded */
+    count = 0;
+    for (i = 0; i < PLAYER_SIZE; ++i)
+        if (players[i].isActive && players[i].hasFolded)
+            count++;
+    
+    if (player_size - count != 1 && game->pot != TOTAL_CASH){ /* everyone has folded go straight to determining winner */
+        /* clear previous better */
+        for (i = 0; i < player_size; ++i)
+            players[i].isBetter = 0;
+        
+        /* determine new better, randomly */
+    find_better:
+        i = rand() % 4;
+        if (!players[i].isActive || players[i].hasFolded)
+            goto find_better;
+        players[i].isBetter = 1;
+        
+        /* make bets */
+        for (i = 0; i < player_size; ++i) {
+            if (players[i].isComputer && players[i].isBetter){
+                bet = rand() % players[i].amount;
+                printf("%s bet %d\n", players[i].name, bet);
+                players[i].amount -= bet;
+                game->pot += bet;
+                
+            }
+            if (!players[i].isComputer && players[i].isBetter){
+                printf("What would you like to bet? (ex: 1, 10, 50), you have: %d\n", players[i].amount);
+            bet:
+                scanf("%s", result);
+                bet = atoi(result);
+                if (bet > players[i].amount || bet < 0){
+                    printf("Too high or too low, please try again\n");
+                    goto bet;
+                }
+                printf("%s bet %d\n", players[i].name, bet);
+                players[i].amount -= bet;
+                game->pot += bet;
+            }
+        }
+        /* if player is not better, he must meet the bet or fold */
+        for (i = 0; i < player_size; ++i) {
+            if (!players[i].hasFolded && players[i].isActive){
+                if (players[i].isComputer && !players[i].isBetter){
+                    if (players[i].amount - bet < 0) {
+                        players[i].hasFolded = 1;
+                        printf("%s has folded\n", players[i].name);
+                    }
+                    else {
+                        players[i].amount -= bet;
+                        printf("%s called the bet\n", players[i].name);
+                        game->pot += bet;
+                    }
+                }
+                if (!players[i].isComputer && !players[i].isBetter){
+                    if (players[i].amount - bet < 0) {
+                        players[i].hasFolded = 1;
+                        printf("%s has folded\n", players[i].name);
+                    }
+                    else {
+                        printf("What would you like to do? Call or Fold? (ex: 'c' or 'f') \n");
+                        scanf("%s", result);
+                        if (!strncmp(result, "c", strlen("c"))){
+                            players[i].amount -= bet;
+                            printf("%s called the bet\n", players[i].name);
+                            game->pot += bet;
+                        }
+                        else {
+                            players[i].hasFolded = 1;
+                            printf("%s has folded\n", players[i].name);
+                        }
+                    }
+                }
+            }
+        }
+        printf("Current pot at: %d\n", game->pot);
+    }
+    
+    free(result);
+}
+
