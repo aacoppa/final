33a510ec990d649f0d470fb033e8496d72ecd038
Sebastian Conybeare <sebmathguy@gmail.com>
2014-01-29 21:08:13 -0500

    Renamed the structs to make sense. We now have
    Rename the structs in types.c to have names that will be more familiar. struct symbol is the top-level thingy that is supposed to passed around by evaluation machinery.

--- Josh_Zane_Sebastian/src/parser/types.c
+++ Josh_Zane_Sebastian/src/parser/types.c
@@ -2,32 +2,34 @@
 #include <stdio.h>
 
 struct llnode;
-struct arg;
+struct symbol;
 struct function;
-struct type;
+union value;
+struct variable;
+
 
 struct llnode {
   struct type* car;
   struct llnode* cdr;
 };
 
-struct arg {
-  char* name;
-  struct type* arg;
+struct symbol {//I guess this is analogous to a symbol in lisp.
+  char* name; //It has a name, which we use to keep track of it.
+  struct variable* referant; //And an actual value in memory.
 };
-
 
 struct function {
-  struct arg* args;
-  char* definition;
+  struct symbol* args; //these are the symbols that the function takes as arguments
+  char* definition; // this is the function's actual definition, left in text form until parsing.
 };
 
-union value {
+union value { //we have three primitive types, so every variable's value is one of those types
   char bit;
   struct llnode list;
+  struct function func;
 };
 
-struct type {
-  union value val;
-  char typeid;
+struct variable { //this is essentially an in-program variable
+  union value val; //it has a value, i.e. what it equals
+  char typeid; //and a type id that allows us to correctly extract the value from the union value.
 };
