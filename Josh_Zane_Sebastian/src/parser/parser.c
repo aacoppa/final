641e00aa3762405a021f7cbe9e9752aa34e01c3e
Sebastian Conybeare <sebmathguy@gmail.com>
2014-01-29 22:08:03 -0500

    Begin mapping out the parser.

---
+++ Josh_Zane_Sebastian/src/parser/parser.c
@@ -0,0 +1,13 @@
+#include types.c
+#include <string.h>
+
+/* We can:
+   Set something equal to something
+   Function calls
+   Function definitions
+   Returning things
+   Printing things
+   Iffing */
+
+struct call parseCall(char* statement);
+struct function parseFunction(char* statement);
