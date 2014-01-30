6e6cfb1ccfdf76509a16ce096fdd5000c318b66e
Sebastian Conybeare <sebmathguy@gmail.com>
2014-01-29 23:19:33 -0500

    Make a function in parser.c that removes duplicate spaces from a string.

--- Josh_Zane_Sebastian/src/parser/parser.c
+++ Josh_Zane_Sebastian/src/parser/parser.c
@@ -5,9 +5,23 @@
    Set something equal to something
    Function calls
    Function definitions
-   Returning things
+   Returning things -- delimiter '<'
    Printing things
    Iffing */
 
 struct call parseCall(char* statement);
 struct function parseFunction(char* statement);
+
+void fixSpacing(char* code) {
+  char* fixedCode = calloc(sizeof(char), strlen(code) + 1);
+  fixedCode = strcpy(fixedCode, code);
+  int n = strlen(fixedCode);
+  char* doubleSpace = strstr(fixedCode, "  ");
+  char* movingIndex;
+  for( ; doubleSpace; doubleSpace = strstr(fixedCode, "  ")) {
+    for(movingIndex = doubleSpace; movingIndex&; movingIndex++) {
+      movingIndex[0] = movingIndex[1];
+    }
+  }
+  return fixedCode;
+}
