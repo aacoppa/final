6245e81dbb7f190c0e290fbc0060115908061111
Sebastian Conybeare <sebmathguy@gmail.com>
2014-01-30 04:50:20 -0500

    did some shit

--- Josh_Zane_Sebastian/src/interpreter/interpreter.c
+++ Josh_Zane_Sebastian/src/interpreter/interpreter.c
@@ -2,60 +2,132 @@
 #include <stdio.h>
 #include <string.h>
 
-void runStep(struct stack_node node, struct stack_node *top) {
+void runStep(struct iq_node node, struct stack_node *top) {
   switch( node.data.type ) {
-  case T_INT: push(node, top);
+  case T_INT:
+    struct stack_node topush;
+    topush.data = node.data;
+    topush.type = node.type;
+    push(topush, top);
     break;
+
   case T_SBRTN:
+    struct stack_node topush;
+    topush.data = node.data;
+    topush.type = node.type;
     push(node, top);
     break;
-  case T_char 1:
+
+
+  case T_CHAR:
     switch( nonde.data.numval ) {
     case '+':
-      int x = pop(top);
-      int y = pop(top);
-      push(x + y);
+      struct stack_node x = pop(top);
+      struct stack_node y = pop(top);
+      x.data += y.data;
+      push(x, top);
       break;
+
     case '_':
-      push(0);
+      struct stack_node x = pop(top);
+      x.data = -(x.data);
+      push(x, top);
+      break;
+
     case '-':
-      int x = pop(top);
-      int y = pop(top);
-      push(x - y);
+      struct stack_node x = pop(top);
+      struct stach_node y = pop(top);
+      x.data -= y.data;
+      push(x, top);
       break;
+
     case '*':
-      int x = pop(top);
-      int y = pop(top);
-      push(x * y);
+      struct stack_node x = pop(top);
+      struct stack_node y = pop(top);
+      x.data *= y.data;
+      push(x, top);
       break;
+
     case '/':
-      int x = pop(top);
-      int y = pop(top);
-      push(x / y);
+      struct stack_node x = pop(top);
+      struct stack_node y = pop(top);
+      x.data /= y.data;
+      push(x, top);
       break;
+
     case '=':
-      int x = pop(top);
-      int y = pop(top);
+      struct stack_node x = pop(top);
+      struct stack_node y = pop(top);
       if(x == y)
-	push(-1);
+	x.data = -1;
       else
-	push(0);
+	x.data = 0;
+      push(x, top);
       break;
+
     case '<':
-      int x = pop(top);
-      int y = pop(top);
+      struct stack_node x = pop(top);
+      struct stack_node y = pop(top);
       if(x < y)
-	push(-1);
+	x.data = -1;
       else
-	push(0);
+	x.data = 0;
+      push(x, top);
       break;
+
+    case '&':
+      struct stack_node x = pop(top);
+      struct stack_node y = pop(top);
+      x.data = -(x.data * y.data);
+      break;
+
     case '~':
-      int x = pop(top);
-      int y = pop(top);
-      if(x == y):
-	push(0);
+      struct stack_node x = pop(top);
+      if(x.data)
+	x.data = 0;
       else
-	push(-1);
+	x.data = -1;
+      push(x, top);
+      break;
+
+    case '|':
+      struct stack_node x = pop(top);
+      struct stack_node y = pop(top);
+      if(x.data)
+	push(x, top);
+      else
+	push(y, top);
+      break;
+
+    case '%':
+      pop(top);
+      break;
+     
+    case '\\':
+      struct stack_node x = pop(top);
+      struct stack_node y = pop(top);
+      push(x, top);
+      push(y, top);
+      break;
+
+    case '@':
+      struct stack_node x = pop(top);
+      struct stack_node y = pop(top);
+      struct stack_node z = pop(top);
+      push(y, top);
+      push(x, top);
+      push(z, top);
+      break;
+
+    case '$':
+      struct stack_node n = pop(top);
+      int i;
+      struct stack_node *x = top;
+      for(i = 0; i < n.data; i++)
+	x = x->cdr;
+      push(*x, top);
+      break;
+      
     }
   }
 }
