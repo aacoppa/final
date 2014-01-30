20f3fbf3c7a108b75c5dc92f0fe1108cc3e9bab5
Zane Sterling <sterling.zane@gmail.com>
2014-01-30 02:36:36 -0500

    Add types, allow for subroutines.

--- Josh_Zane_Sebastian/src/main.h
+++ Josh_Zane_Sebastian/src/main.h
@@ -1,6 +1,15 @@
+#define T_INT 0
+#define T_CHAR 1
+#define T_SBRTN 2
+
 struct stack_node {
     struct stack_node *cdr;
-    int data;
+    union node_data data;
+}
+
+union node_data {
+    struct subroutine srtine;
+    int numval;
     char type;
 }
 
