9d61ff604852019b1958b719f0f48b099566f14c
Zane Sterling <sterling.zane@gmail.com>
2014-01-29 21:12:03 -0500

    Add a function to the shit with builtins

--- Josh_Zane_Sebastian/src/interpreter/interpreter.c
+++ Josh_Zane_Sebastian/src/interpreter/interpreter.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "dictionary.c"
 
 // interprets a parsed version of the program, split into statements
 void interpret(struct program* program) {
@@ -11,7 +12,7 @@ void interpret(struct program* program) {
       // a = | b c
       assign(stmt.parts[1], stmt.parts[0]);
     } else if (stmt.type == ST_RETRN) {
-      // > $| b c
+      // < $| b c
       retVal(stmt.parts[0]);
     } else if (stmt.type == ST_FUNCT) {
       // | b c
@@ -24,15 +25,41 @@ void interpret(struct program* program) {
 void runFunc(struct fnCall* fnCall) {
   int i;
 
+  // create futures where necessary
   for (i = 0; i < fnCall->numArgs; i++) {
     if (!isAtomic(fnCall->args[i])) {
       setArg(fnCall, i, future(fnCall, i));
     }
   }
 
+  // wait for the futures to be evaluated
   for (i = 0; i < fnCall->numArgs; i++) {
-    while(!(fnCall->args[i].fulfilled));
+    while(!(fnCall->args[i].fulfilled)) {
+      usleep(100 * 1000); // sleep for 0.1s
+    }
   }
 
-  // run the function somehow
+  // if the function is a builtin, run that builtin
+  if (isBuiltin(fnCall)) {
+    runBuiltin(fnCall);
+  } else {
+    // TODO run the function somehow
+  }
+}
+
+// tests to see if a function is one of the builtins
+int isBuiltin(struct fnCall* fnCall) {
+  char *name = fnCall->name;
+
+  if (!strcmp(name, "!") || !strcmp(name, "|")) {
+    return 1;
+  }
+
+  if (!strcmp(name, "[") || !strcmp(name, "]")) {
+    return 1;
+  }
+
+  if (!strcmp(name, ">") || !strcmp(name, "?")) {
+    return 1;
+  }
 }
