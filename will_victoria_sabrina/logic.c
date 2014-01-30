1dd425e7f353f720d1941fecb86b4801b9260cd1
Victoria Greene <topaz.q98@gmail.com>
2014-01-29 22:15:39 -0500

    completed all functions in logic.c and updates the headers in logic.h

--- will_victoria_sabrina/logic.c
+++ will_victoria_sabrina/logic.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "model.h"
 #include <string.h>
+
 // check if territories are adjacent
 char tadjacent(territory *t1, territory *t2) {
   int *p = t1->neighbors;
@@ -11,4 +12,66 @@ char tadjacent(territory *t1, territory *t2) {
     p++;
   }
   return 0;
+}
+
+int die_roll() {
+  int roll = (int)(arc4random() % 6 + 1);
+  return roll;
+}
+
+int battle(int uAtt, int uDef) {
+  while (uAtt > 0 && uDef > 0) {
+    int aRoll = die_roll();
+    int dRoll = die_roll();
+    if (aRoll > dRoll) {
+      uDef--;
+    }
+    else {
+      uAtt--;
+    }
+  }
+  if (uAtt == 0) {
+    return 1;
+  }
+  return 0;
+}
+
+void distribute(int numPlayers) {
+  int counter = 0;
+  int temp[42];
+  while (counter <= 41) {
+    *(temp + counter) = counter;
+    counter++;
+  }
+  int curr = 1;
+  counter = 0;
+  while (counter <= 41) {
+    int a = (int)(arc4random() % 42);
+    if (*(temp + a) != -1) {
+      ((territory)*(terrs + *(temp + a))).owner = curr;
+      *(temp + a) = -1;
+      if (curr == numPlayers) {
+	curr = 1;
+      }
+      else {
+	curr++;
+      }
+      counter++;
+    }
+  }
+}
+
+int main() {
+  printf("ROLL: %d\n", die_roll());
+  printf("%d\n", battle(5,2));
+  printf("%d\n", battle(2,5));
+  printf("%d\n", battle(7,1));
+  printf("%d\n", battle(1,7));
+  printf("%d\n", battle(3,3));
+  printf("%d\n", battle(3,3));
+  printf("%d\n", battle(3,3));
+  printf("%d\n", battle(3,3));
+  printf("%d\n", battle(3,3));
+  printf("%d\n", battle(3,3));
+  printf("%d\n", battle(3,3));
 }
